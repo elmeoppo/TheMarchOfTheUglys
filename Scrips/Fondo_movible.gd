f6f31400e39cc7283d1f7b0389cc3f98e686f42d
Isaac Salas <iscprepa@gmail.com>
2026-03-15 21:47:36 -0600

    Parallax

--- Scrips/Fondo_movible.gd
+++ Scrips/Fondo_movible.gd
@@ -4,10 +4,13 @@ class_name FollowingBack
 @export var velocity_multiplier : float = 1.0
 var direction_player : float
 
+var initial_pos
 
 func _ready() -> void:
+	print(initial_pos)
 	jugador.moving.connect(move_back)
 	jugador.stop.connect(stop_move_back)
+	
 
 func move_back(direction : float):
 	direction_player = direction
@@ -16,6 +19,6 @@ func move_back(direction : float):
 func stop_move_back():
 	direction_player = 0
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if direction_player != 0:
 		scroll_offset.x -= (direction_player * velocity_multiplier)
