f4fd4761432885a56921168eef94caee127bf9ac
elmeoppo <elmeoppoo@gmail.com>
2026-03-15 21:26:14 -0500

    comit_22

---
+++ escenas/path_follow_2d.gd
@@ -0,0 +1,12 @@
+extends PathFollow2D
+
+@export var velocidad_medusa : float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress += velocidad_medusa*delta
